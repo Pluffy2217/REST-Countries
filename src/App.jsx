9cbccdb2c673202dc3ce234c82d1f6345cf52b1c
Pluffy2217 <prathamkl17@gmail.com>
2023-08-10 18:38:47 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const  App = () => {
   return (    
     <Routes>
       <Route path='/' element={<Home />}></Route>
-      <Route path={tld} element={<AboutCountry />}></Route>
+      <Route path={`/about`} element={<AboutCountry />}></Route>
     </Routes>
   )
 }
