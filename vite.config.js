b3d6d4cf8f968c27f948be93cc17cb5fe371a915
Pluffy2217 <prathamkl17@gmail.com>
2023-08-10 18:51:59 +0530

    Fourth Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:80}
+  server: {port:80},
+  build: {
+    rollupOptions: {
+      external: ["react", "react-router", "react-router-dom", "react-redux"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  }
 })
